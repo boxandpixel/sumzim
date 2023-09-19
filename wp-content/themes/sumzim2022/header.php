a69b9c574b05eece5c31dbb847dc97cce2dcb994
Brian Gunzenhauser <brian@boxandpixel.com>
2023-09-19 14:12:19 -0400

    Test push 2:12pm

--- wp-content/themes/sumzim2022/header.php
+++ wp-content/themes/sumzim2022/header.php
@@ -13,7 +13,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<!-- test push 20230919 12:43pm -->
+	<!-- test push 20230919 2:12pm -->
 	<meta charset="utf-8">	
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
