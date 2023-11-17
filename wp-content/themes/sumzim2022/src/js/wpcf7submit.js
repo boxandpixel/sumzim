5b359359781bbda5ed2da9aa7dc6659fd394b067
Brian Gunzenhauser <briangunzenhauser@Brians-MacBook-Pro-2023.local>
2023-11-17 12:02:37 -0500

    Redirects for all forms

--- wp-content/themes/sumzim2022/src/js/wpcf7submit.js
+++ wp-content/themes/sumzim2022/src/js/wpcf7submit.js
@@ -1,21 +1,64 @@
 document.addEventListener("DOMContentLoaded", ()=> {
     const freeEstimateForm = document.getElementById("wpcf7-f4179-o1");
+    const joeForm = document.getElementById("wpcf7-f4161-o1");
+    const contactUsForm = document.getElementById("wpcf7-f4338-o1");
+    const careerHvacForm = document.getElementById("wpcf7-f4262-o1");
+    const careerPlumbingForm = document.getElementById("wpcf7-f4268-o1");
+    const careerGeneralForm = document.getElementById("wpcf7-f4273-o1");
+    const designYourMembership = document.getElementById("wpcf7-f28026-o1");
 
 
     if(freeEstimateForm) {
-
-        console.log("Hello Free Estimate form");
-
-        freeEstimateForm.addEventListener('wpcf7submit', (e)=> {
-            console.log("submitted");
-        })
-
-        freeEstimateForm.addEventListener('wpcf7mailsent', (e)=> {
-            console.log("mailed");
-            window.location.replace("https://sumzimdev.wpengine.com/request-confirmation?form=FreeEstimateForm");
-        })
+         freeEstimateForm.addEventListener('wpcf7mailsent', ()=> {
+            window.location.replace("https://sumzimdev.wpengine.com/request-confirmation?form=Free%20Estimate");
+        });
     } 
-});
+
+    // Joe Form
+    if(joeForm) {
+        joeForm.addEventListener('wpcf7mailsent', ()=> {
+            window.location.replace("https://sumzimdev.wpengine.com/request-confirmation?form=Contact%20Joe%20Zimmerman");
+        });
+    }     
+
+    // Contact Us Form
+    if(contactUsForm) {
+        contactUsForm.addEventListener('wpcf7mailsent', ()=> {
+            window.location.replace("https://sumzimdev.wpengine.com/request-confirmation?form=Contact%20Us");
+        });
+    }
+
+    // HVAC Application
+    if(careerHvacForm) {
+        careerHvacForm.addEventListener('wpcf7mailsent', ()=> {
+            window.location.replace("https://sumzimdev.wpengine.com/request-confirmation?form=HVAC%20Application");
+        });
+    }
+
+    // Plumber Application
+    if(careerPlumbingForm) {
+        careerPlumbingForm.addEventListener('wpcf7mailsent', ()=> {
+            window.location.replace("https://sumzimdev.wpengine.com/request-confirmation?form=Plumbing%20Application");
+        });
+    }
+
+    // General Application
+    if(careerGeneralForm) {
+        careerGeneralForm.addEventListener('wpcf7mailsent', ()=> {
+            window.location.replace("https://sumzimdev.wpengine.com/request-confirmation?form=General%20Application");
+        });
+    }
+
+    // Membership Design
+    if(designYourMembership) {
+        designYourMembership.addEventListener('wpcf7mailsent', ()=> {
+            window.location.replace("https://sumzimdev.wpengine.com/request-confirmation?form=Design%20Your%20Membership");
+        });
+    }
+
+
+});   
+
 
 
 
