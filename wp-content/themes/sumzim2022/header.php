650e35a6b7f1154cc6b24fff1824330ff67ff9c4
Box + Pixel <brian@boxandpixel.com>
2024-08-26 08:58:01 -0400

    Add second GTM to site

--- wp-content/themes/sumzim2022/header.php
+++ wp-content/themes/sumzim2022/header.php
@@ -29,6 +29,15 @@
 
 	<?php wp_head(); ?>
 
+	<!-- GTM #2 20240826 -->
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-NFL8X42V');</script>
+	<!-- End Google Tag Manager -->	 	
+
   <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -161,6 +170,12 @@
 <!-- Test push 20240716 -->
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+	<!-- GTM #2 20240826 -->
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NFL8X42V"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->	 
+
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJT6M7F"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
