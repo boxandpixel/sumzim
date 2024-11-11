f703f4ed07525e51621a2719a07fbc45d3ef34fd
Box + Pixel <brian@boxandpixel.com>
2024-11-11 14:18:20 -0500

    Fix GTM

--- wp-content/themes/sumzim2022/header.php
+++ wp-content/themes/sumzim2022/header.php
@@ -29,24 +29,14 @@
 
 	<?php wp_head(); ?>
 
-<<<<<<< HEAD
-	<!-- Google Tag Manager -->
+	<!-- GTM -->
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	})(window,document,'script','dataLayer','GTM-NFL8X42V');</script>
-	<!-- End Google Tag Manager -->	 	
-=======
-  <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MJT6M7F');</script>
-  <!-- End Google Tag Manager -->
+	<!-- End Google Tag Manager -->	 
 
->>>>>>> yt
 
 
 <!-- Google Fonts -->
