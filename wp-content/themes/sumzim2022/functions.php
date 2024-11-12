965dc51c41e133d894abf2298f98837bd7417d8e
Box + Pixel <brian@boxandpixel.com>
2024-11-12 14:40:00 -0500

    Revert to old GTM code

--- wp-content/themes/sumzim2022/functions.php
+++ wp-content/themes/sumzim2022/functions.php
@@ -174,6 +174,12 @@ function sumzim_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'sumzim_scripts' );
+
+function my_plugin_partytown_config( $config ) {
+	$config["forward"] = ['dataLayer.push'];
+	return $config;
+  }
+  add_filter( 'partytown_configuration', 'my_plugin_partytown_config' );
   
 /** 
  * Remove versions from CSS & JS
