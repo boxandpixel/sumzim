726d424a11939a7aef0eaa2cc88427dcc24f9bfc
Box + Pixel <brian@boxandpixel.com>
2025-01-16 09:06:01 -0500

    Add stack adapt pixels

--- wp-content/themes/sumzim2022/header.php
+++ wp-content/themes/sumzim2022/header.php
@@ -43,7 +43,12 @@
 	<!-- End StackAdapt Universal Pixel -->
 
 	<!-- Start StackAdapt Click to Call Pixel -->
-	<script> var saClickToCall = document.getElementById("sa-click-to-call"); saClickToCall.addEventListener("click",function() { !function(s,a,e,v,n,t,z){if(s.saq)return;n=s.saq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!s._saq)s._saq=n;n.push=n;n.loaded=!0;n.version='1.0';n.queue=[];t=a.createElement(e);t.async=!0;t.src=v;z=a.getElementsByTagName(e)[0];z.parentNode.insertBefore(t,z)}(window,document,'script','https://tags.srv.stackadapt.com/events.js');saq('conv', 'cuJArc7ZMDOyfyEhCMMMuU'); }); </script>
+	<script> var saClickToCall = document.getElementById("sa-click-to-call"); 
+			if(saClickToCall) {
+				saClickToCall.addEventListener("click",function() { !function(s,a,e,v,n,t,z){if(s.saq)return;n=s.saq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!s._saq)s._saq=n;n.push=n;n.loaded=!0;n.version='1.0';n.queue=[];t=a.createElement(e);t.async=!0;t.src=v;z=a.getElementsByTagName(e)[0];z.parentNode.insertBefore(t,z)}(window,document,'script','https://tags.srv.stackadapt.com/events.js');saq('conv', 'cuJArc7ZMDOyfyEhCMMMuU'); }); 
+			}
+	</script>
+			
 	<!-- End StackAdapt Click to Call Pixel -->
 
 
