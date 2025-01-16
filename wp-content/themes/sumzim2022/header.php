3194768a448470c9e79e28de00b9fad15cfe74f3
Box + Pixel <brian@boxandpixel.com>
2025-01-16 09:16:36 -0500

    Add stack adapt pixels

--- wp-content/themes/sumzim2022/header.php
+++ wp-content/themes/sumzim2022/header.php
@@ -43,10 +43,10 @@
 	<!-- End StackAdapt Universal Pixel -->
 
 	<!-- Start StackAdapt Click to Call Pixel -->
-	<script> var saClickToCall = document.getElementById("sa-click-to-call"); 
-			if(saClickToCall) {
-				saClickToCall.addEventListener("click",function() { !function(s,a,e,v,n,t,z){if(s.saq)return;n=s.saq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!s._saq)s._saq=n;n.push=n;n.loaded=!0;n.version='1.0';n.queue=[];t=a.createElement(e);t.async=!0;t.src=v;z=a.getElementsByTagName(e)[0];z.parentNode.insertBefore(t,z)}(window,document,'script','https://tags.srv.stackadapt.com/events.js');saq('conv', 'cuJArc7ZMDOyfyEhCMMMuU'); }); 
-			}
+	<script> 
+		document.getElementById("sa-click-to-call--m").addEventListener("click",function() { !function(s,a,e,v,n,t,z){if(s.saq)return;n=s.saq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!s._saq)s._saq=n;n.push=n;n.loaded=!0;n.version='1.0';n.queue=[];t=a.createElement(e);t.async=!0;t.src=v;z=a.getElementsByTagName(e)[0];z.parentNode.insertBefore(t,z)}(window,document,'script','https://tags.srv.stackadapt.com/events.js');saq('conv', 'cuJArc7ZMDOyfyEhCMMMuU'); });
+
+		document.getElementById("sa-click-to-call--d").addEventListener("click",function() { !function(s,a,e,v,n,t,z){if(s.saq)return;n=s.saq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!s._saq)s._saq=n;n.push=n;n.loaded=!0;n.version='1.0';n.queue=[];t=a.createElement(e);t.async=!0;t.src=v;z=a.getElementsByTagName(e)[0];z.parentNode.insertBefore(t,z)}(window,document,'script','https://tags.srv.stackadapt.com/events.js');saq('conv', 'cuJArc7ZMDOyfyEhCMMMuU'); });
 	</script>
 			
 	<!-- End StackAdapt Click to Call Pixel -->
@@ -229,7 +229,7 @@
 			</div>		
 		</div>
 		<div class="header__mobile-call">
-			<h2>Call us 24/7 at <a href="tel:6105935129">610-593-5129</a></h2>
+			<h2>Call us 24/7 at <a href="tel:6105935129" id="sa-click-to-call--m">610-593-5129</a></h2>
 		</div>
 		<?php endif; ?>
 		<header class="site-header">
@@ -287,7 +287,7 @@
 						</a>
 					</div>
 				</div>
-				<p class="header__call">Call us 24/7 at <a href="tel:+16105935129">(610) 593-5129</a></p>
+				<p class="header__call">Call us 24/7 at <a href="tel:+16105935129" id="sa-click-to-call--d">(610) 593-5129</a></p>
 
 				<nav class="header__nav">
 					<?php
