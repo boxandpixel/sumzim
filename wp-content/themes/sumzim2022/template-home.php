4b5c43c28381dad58e808f7c38901d28dd7a2806
Box + Pixel <brian@boxandpixel.com>
2025-01-31 13:58:35 -0500

    Fix home page button

--- wp-content/themes/sumzim2022/template-home.php
+++ wp-content/themes/sumzim2022/template-home.php
@@ -42,7 +42,7 @@ get_header();
 					<a href="tel:6105935129" class="button button-cta button--schedule button--large book-now-button" id="sa-click-to-call">Call 610-593-5129</a>
 					<div class="home__hero-link-mobile-alt">
 						or <a href="https://book.servicetitan.com/rodibizoajr2ydb24uw8s8x1?hl=en-US&gei=yhqdZ5W4OOjbptQP2LfRqA8&rwg_token=AAiGsoZe4ivBv_kqeIdkCTrg6eIJNfP2Ua2hQ-6jpoSUWDkpjd6tqFC7_hbu4o1_xLCztJazn15sMaNXLNElhKeI4wCveqVnQA%3D%3D" 
-                  class="se-widget-button se-button-alt">Book Now</a>
+                  class="se-widget-button se-button-alt" target="_blank">Book Now</a>
 					</div>
 				</div>
 				
