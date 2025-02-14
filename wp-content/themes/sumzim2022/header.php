782fa539cde5886d5eb95c66831a6a13fc0a87d9
Box + Pixel <brian@boxandpixel.com>
2025-02-14 15:52:49 -0500

    Fix p margin in .entry-content for default page template

--- wp-content/themes/sumzim2022/header.php
+++ wp-content/themes/sumzim2022/header.php
@@ -43,7 +43,7 @@
 	<!-- End StackAdapt Universal Pixel -->
 
 	<!-- Start StackAdapt Click to Call Pixel -->
-	<script> 
+	<script>
 		document.getElementById("sa-click-to-call").addEventListener("click",function() { !function(s,a,e,v,n,t,z){if(s.saq)return;n=s.saq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!s._saq)s._saq=n;n.push=n;n.loaded=!0;n.version='1.0';n.queue=[];t=a.createElement(e);t.async=!0;t.src=v;z=a.getElementsByTagName(e)[0];z.parentNode.insertBefore(t,z)}(window,document,'script','https://tags.srv.stackadapt.com/events.js');saq('conv', 'cuJArc7ZMDOyfyEhCMMMuU'); });
 	</script>
 			
