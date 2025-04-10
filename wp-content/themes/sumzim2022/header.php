849f6eb54b9287b930c49fa1001410f0d42b1352
Box + Pixel <brian@boxandpixel.com>
2025-04-10 11:41:47 -0400

    Test push

--- wp-content/themes/sumzim2022/header.php
+++ wp-content/themes/sumzim2022/header.php
@@ -13,7 +13,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<!-- Test 20250410 11:16am -->
+	<!-- Test 20250410 11:41am -->
 	<meta charset="utf-8">	
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
