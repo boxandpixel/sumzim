e9987dadb9d09e1fe0778a16e5bf1b01a9b49370
Box + Pixel <brian@boxandpixel.com>
2025-06-02 16:41:00 -0400

    Add link to image section

--- wp-content/themes/sumzim2022/blocks/image-section/image-section.php
+++ wp-content/themes/sumzim2022/blocks/image-section/image-section.php
@@ -11,12 +11,23 @@ $button = $content_group['button'];
 $image_group = $image_section['image_group'];
 $image = $image_group['image'];
 $image_caption = $image_group['image_caption'];
+$image_link = $image_group['image_link'];
+
+if(!empty($image_link)): 
+	$image_link_target = $image_link['target'] ? $image_link['target'] : "_self";
+endif;	
 ?>
 
 
 <section class="image-section-block contain">
 	<figure>
+		<?php if(!empty($image_link)): ?>
+		<a href="<?= esc_url($image_link['url']); ?>" target="<?= esc_attr($image_link_target); ?>">
+		<?php endif; ?>
 		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+		<?php if(!empty($image_link)): ?>
+		</a>
+		<?php endif; ?>		
 		<?php if($image_caption): ?>
 		<figcaption><?php echo $image_caption; ?></figcaption>
 		<?php endif; ?>
