f98e1c376aee76c1d79c5b191125238ba8d21734
Box + Pixel <brian@boxandpixel.com>
2026-03-24 14:24:14 -0400

    fix: update mem landing eval form urls

--- wp-content/themes/sumzim2022/template-blue-membership-plans.php
+++ wp-content/themes/sumzim2022/template-blue-membership-plans.php
@@ -95,14 +95,14 @@ get_header();
 			<h2>Example Evaluation Sheets</h2>
 
 			<ul>
-				<li><a href="https://stg-summerszims-staging.kinsta.cloud/wp-content/uploads/2026/03/membership-eval-gas-furnace-redacted.pdf" target="_blank">Example Gas Furnace Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
-				<li><a href="https://stg-summerszims-staging.kinsta.cloud/wp-content/uploads/2026/03/membership-eval-oil-inspection.pdf" target="_blank">Example Oil Boiler/Furnace Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
-				<li><a href="https://stg-summerszims-staging.kinsta.cloud/wp-content/uploads/2026/03/membership-eval-heat-pump-ac-redacted.pdf" target="_blank">Example Heat Pump Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
-				<li><a href="https://stg-summerszims-staging.kinsta.cloud/wp-content/uploads/2026/03/membership-eval-heat-pump-ac-redacted.pdf" target="_blank">Example A/C Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
-				<li><a href="https://stg-summerszims-staging.kinsta.cloud/wp-content/uploads/2026/03/membership-eval-water-treatment.pdf" target="_blank">Example Water Treatment Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
-				<li><a href="https://stg-summerszims-staging.kinsta.cloud/wp-content/uploads/2026/03/membership-eval-generator-checklist.pdf" target="_blank">Example Generator Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
-				<li><a href="https://stg-summerszims-staging.kinsta.cloud/wp-content/uploads/2026/03/membership-eval-electrical-safety.pdf" target="_blank">Example Electrical Safety Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
-				<li><a href="https://stg-summerszims-staging.kinsta.cloud/wp-content/uploads/2026/03/membership-eval-backflow-test.pdf" target="_blank">Example Backflow Preventer Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
+				<li><a href="https://sumzim.com/wp-content/uploads/2026/03/membership-eval-gas-furnace-redacted.pdf" target="_blank">Example Gas Furnace Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
+				<li><a href="https://sumzim.com/wp-content/uploads/2026/03/membership-eval-oil-inspection.pdf" target="_blank">Example Oil Boiler/Furnace Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
+				<li><a href="https://sumzim.com/wp-content/uploads/2026/03/membership-eval-heat-pump-ac-redacted.pdf" target="_blank">Example Heat Pump Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
+				<li><a href="https://sumzim.com/wp-content/uploads/2026/03/membership-eval-heat-pump-ac-redacted.pdf" target="_blank">Example A/C Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
+				<li><a href="https://sumzim.com/wp-content/uploads/2026/03/membership-eval-water-treatment.pdf" target="_blank">Example Water Treatment Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
+				<li><a href="https://sumzim.com/wp-content/uploads/2026/03/membership-eval-generator-checklist.pdf" target="_blank">Example Generator Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
+				<li><a href="https://sumzim.com/wp-content/uploads/2026/03/membership-eval-electrical-safety.pdf" target="_blank">Example Electrical Safety Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
+				<li><a href="https://sumzim.com/wp-content/uploads/2026/03/membership-eval-backflow-test.pdf" target="_blank">Example Backflow Preventer Evaluation Sheet <span class="material-symbols-outlined">open_in_new</span></a></li>
 			</ul>
 		</div>		
 
