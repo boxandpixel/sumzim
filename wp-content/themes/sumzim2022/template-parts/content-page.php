7ab92c327bbc8545a03c2fed29b602be8c66f977
Box + Pixel <brian@boxandpixel.com>
2026-04-16 11:33:21 -0400

    feat: add breadcrumb to pages

--- wp-content/themes/sumzim2022/template-parts/content-page.php
+++ wp-content/themes/sumzim2022/template-parts/content-page.php
@@ -7,13 +7,34 @@
  * @package _s
  */
 
-$page_settings = get_field('page_settings');
-$seo_title = $page_settings['seo_title'] ?? '';
+$page_settings      = get_field('page_settings');
+$seo_title          = $page_settings['seo_title'] ?? '';
+$show_breadcrumb    = $page_settings['show_breadcrumb'] ?? false;
+
+$parent_id = wp_get_post_parent_id( get_the_ID() );
+if ( $parent_id ) {
+	$breadcrumb_parent_url   = get_permalink( $parent_id );
+	$breadcrumb_parent_label = get_the_title( $parent_id );
+} else {
+	$breadcrumb_parent_url   = home_url( '/' );
+	$breadcrumb_parent_label = 'Home';
+}
 
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+
+		<?php if ( $show_breadcrumb ) : ?>
+		<div class="container entry-header__breadcrumb">
+			<nav class="blog-breadcrumb" aria-label="Breadcrumb">
+				<a href="<?= esc_url( $breadcrumb_parent_url ); ?>" class="blog-breadcrumb__parent"><?= esc_html( $breadcrumb_parent_label ); ?></a>
+				<span class="blog-breadcrumb__sep">/</span>
+				<span class="blog-breadcrumb__current"><?= esc_html( get_the_title() ); ?></span>
+			</nav>
+		</div>
+		<?php endif; ?>
+
 		<h1><?= $seo_title ?: get_the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
